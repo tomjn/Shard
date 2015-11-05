77dc9e359a57bfc08de8fd512b77a9bb08994ada
Tom J Nowell <contact@tomjn.com>
2015-11-05 03:21:20 +0000

    remove trailing spaces

--- data/ai/preload/api.lua
+++ data/ai/preload/api.lua
@@ -8,12 +8,12 @@ require "aibase"
 
 game = {}
 	--game_engine
-	
+
 	-- prints 'message' to ingame chat console
 	function game:SendToConsole(message)
 		return game_engine:SendToConsole(message)
 	end
-	
+
 	function game:Frame() -- returns int/game frame number
 		if Spring ~= nil then
 			return Spring.GetGameFrame() -- Spring Gadget API
@@ -21,42 +21,42 @@ game = {}
 			return game_engine:Frame() -- Shard AI API
 		end
 	end
-	
+
 	function game:Test() -- debug
 		return game_engine:Test()
 	end
-	
+
 	function game:IsPaused() -- if the game is paused, returns true
 		--
 		return game_engine:IsPaused()
 	end
-	
+
 	function game:GetTypeByName(typename) -- returns unittype
 		--
 		return game_engine:GetTypeByName(typename)
 	end
-	
-	
+
+
 	function game:ConfigFolderPath() -- returns string with path to the folder
 		--
 		return game_engine:ConfigFolderPath()
 	end
-	
+
 	function game:ReadFile(filename) -- returns string with file contents
 		--
 		return game_engine:ReadFile(filename)
 	end
-	
+
 	function game:FileExists(filename) -- returns boolean
 		--
 		return game_engine:FileExists(filename)
 	end
-	
+
 	function game:GetTeamID() -- returns boolean
 		--
 		return game_engine:GetTeamID()
 	end
-	
+
 	function game:GetEnemies()
 		local has_enemies = game_engine:HasEnemies()
 		if has_enemies ~= true then
@@ -176,12 +176,12 @@ map = {}
 		--
 		return game_engine:Map():FindClosestBuildSite(unittype,builderpos, searchradius, minimumdistance)
 	end
-	
+
 	function map:CanBuildHere(unittype,position) -- returns boolean
 		--
 		return game_engine:Map():CanBuildHere(unittype,position)
 	end
-	
+
 	function map:GetMapFeatures()
 		local fv = game_engine:Map():GetMapFeatures()
 		local f = {}
@@ -193,7 +193,7 @@ map = {}
 		fv = nil
 		return f
 	end
-	
+
 	function map:GetMapFeaturesAt(position,radius)
 		local m = game_engine:Map()
 		local fv = m:GetMapFeaturesAt(position,radius)
@@ -202,7 +202,7 @@ map = {}
 		while i  < fv:size() do
 			table.insert(f,fv[i])
 			i = i + 1
-		end 
+		end
 		fv = nil
 		return f
 	end
@@ -211,12 +211,12 @@ map = {}
 		local m = game_engine:Map()
 		return m:SpotCount()
 	end
-	
+
 	function map:GetSpot(idx) -- returns a Position for the given spot
 		local m = game_engine:Map()
 		return m:GetSpot(idx)
 	end
-	
+
 	function map:GetMetalSpots() -- returns a table of spot positions
 		--
 		local m = game_engine:Map()
@@ -227,32 +227,32 @@ map = {}
 		while i  < count do
 			table.insert( f, m:GetSpot(i) )
 			i = i + 1
-		end 
+		end
 		--fv = nil
 		return f
 	end
-	
+
 	function map:MapDimensions() -- returns a Position holding the dimensions of the map
 		local m = game_engine:Map()
 		return m:MapDimensions()
 	end
-	
+
 	function map:MapName() -- returns the name of this map
 		local m = game_engine:Map()
 		return m:MapName()
 	end
-	
+
 	function map:AverageWind() -- returns (minwind+maxwind)/2
 		local m = game_engine:Map()
 		return m:AverageWind()
 	end
-	
-	
+
+
 	function map:MinimumWindSpeed() -- returns minimum windspeed
 		local m = game_engine:Map()
 		return m:MinimumWindSpeed()
 	end
-	
+
 	function map:MaximumWindSpeed() -- returns maximum wind speed
 		local m = game_engine:Map()
 		return m:MaximumWindSpeed()
@@ -267,13 +267,13 @@ map = {}
 		local m = game_engine:Map()
 		return m:MinimumHeight()
 	end
-	
+
 
 	function map:TidalStrength() -- returns tidal strength
 		local m = game_engine:Map()
 		return m:TidalStrength()
 	end
-	
+
 	game.map = map
 --}
 --[[
@@ -290,14 +290,14 @@ IUnit/ engine unit objects
 
 	void Forget() // makes the interface forget about this unit and cleanup
 	bool Forgotten() // for interface/debugging use
-	
+
 	IUnitType* Type()
 
 	bool CanMove()
 	bool CanDeploy()
 	bool CanBuild()
 	bool IsBeingBuilt()
-	
+
 	bool CanAssistBuilding(IUnit* unit)
 
 	bool CanMoveWhenDeployed()
@@ -320,7 +320,7 @@ IUnit/ engine unit objects
 	bool Attack(IUnit* unit)
 	bool Repair(IUnit* unit)
 	bool MorphInto(IUnitType* t)
-	
+
 	Position GetPosition()
 	float GetHealth()
 	float GetMaxHealth()
@@ -344,9 +344,9 @@ IUnit/ engine unit objects
 		function CanBuildWhenNotDeployed() -- returns boolean
 
 		function Extractor() -- returns boolean
-		
+
 		function GetMaxHealth() -- returns a float
-		
+
 		function WeaponCount() -- returns integer
 	},
 	MapFeature {
