c3a32790537b4cfec6a755f137025563c6d94723
Anarchid <anarchid@mail.ru>
2015-11-26 16:38:00 +0200

    Revert abolition of sethaven: fixed in game.
    This reverts commit 000408550ad6ead838191773f18bb40a89eaadf1.

--- data/ai/ZK/assistbehaviour.lua
+++ data/ai/ZK/assistbehaviour.lua
@@ -37,10 +37,21 @@ function AssistBehaviour:UnitBuilt(unit)
 			local upos = RandomAway(self.unit:Internal():GetPosition(), 50)
 			local floats = api.vectorFloat()
 			-- populate with x, y, z of the position
+			self.havenPos = upos
 			floats:push_back(upos.x)
 			floats:push_back(upos.y)
 			floats:push_back(upos.z)
 			self.unit:Internal():ExecuteCustomCommand(CMD_PATROL, floats)
+			game:SendToContent("sethaven|"..upos.x..'|'..upos.y..'|'..upos.z);
+		end
+	end
+end
+
+function AttackerBehaviour:UnitDead(unit)
+	if unit.engineID == self.unit.engineID then
+		if(self.havenPos) then
+			local upos = self.havenPos
+			game:SendToContent("sethaven|"..upos.x..'|'..upos.y..'|'..upos.z);
 		end
 	end
 end
