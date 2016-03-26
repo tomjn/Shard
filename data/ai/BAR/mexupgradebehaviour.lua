8a2fa46a6734da2bb3c46426b24d1a1f7645b0b1
pandaro <padarogames@gmail.com>
2016-03-26 15:57:04 +0100

    fix water mex upgrade process

--- data/ai/BAR/mexupgradebehaviour.lua
+++ data/ai/BAR/mexupgradebehaviour.lua
@@ -44,7 +44,7 @@ function MexUpgradeBehaviour:UnitIdle(unit)
 					mohoName = "armmoho"
 				end
 				-- maybe we're underwater?
-				tmpType = game:GetTypeByName("comuwmme")
+				tmpType = game:GetTypeByName("coruwmme")
 				if builder:CanBuild(tmpType) then
 					mohoName = "coruwmme"
 				end
