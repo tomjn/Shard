df51bf06d2af5b42772d44f6b2562d990658b534
Tom J Nowell <contact@tomjn.com>
2016-10-02 01:00:47 +0100

    adjustments to help with native Shard

--- data/ai/preload/globals.lua
+++ data/ai/preload/globals.lua
@@ -3,24 +3,31 @@
 if ShardSpringLua then
 	require "preload/spring_lua/boot"
 elseif game_engine then
-	require "preload/spring_native/boot"
+	require "spring_native/boot"
 else
 	require "preload/shard_null/boot"
 end
 
-require "preload/hooks"
-require "preload/class"
-require "preload/aibase"
 
 if ShardSpringLua then
+	require "preload/hooks"
+	require "preload/class"
+	require "preload/aibase"
 	require "preload/spring_lua/unit"
 	require "preload/spring_lua/unittype"
 	require "preload/spring_lua/damage"
 	require "preload/spring_lua/feature"
 	require "preload/spring_lua/controlpoint"
 elseif game_engine then
+	shard_include "hooks"
+	shard_include "class"
+	shard_include "aibase"
+
 	shard_include "preload/spring_native/unit"
 	shard_include "preload/spring_native/unittype"
 else
 	-- load null objects
+	require "preload/hooks"
+	require "preload/class"
+	require "preload/aibase"
 end
