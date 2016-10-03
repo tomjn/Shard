d95f1e8879d252e758199e55843b4a11db705af9
Tom J Nowell <contact@tomjn.com>
2016-10-03 22:03:28 +0100

    attempt to recurse into subdirectories and syntax check lua files

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,17 @@
 cd data/ai
 ../../lua/lua boot.lua
+
+for file in `find .`
+do
+    EXTENSION="${file##*.}"
+
+    if [ "$EXTENSION" == "lua" ]
+    then
+        RESULTS=`../../luac -p $file`
+
+        if [ "$RESULTS" != "No syntax errors detected in $file" ]
+        then
+            echo $RESULTS
+        fi
+    fi
+done
