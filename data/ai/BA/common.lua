205a6fb9094d46aecc32c3134b99bec01ee6ef57
pandaro <prandipadaro@gmail.com>
2017-05-10 21:05:22 +0200

    start cleaning from useless shardlua internal check

--- data/ai/BA/common.lua
+++ data/ai/BA/common.lua
@@ -1,8 +1,4 @@
 shard_include "unitlists"
-if ShardSpringLua then
-	unitTable, featureTable = shard_include("getunitfeaturetable")
-else
-	shard_include("unittable-" .. game:GameName())
-	shard_include("featuretable-" .. game:GameName())
-end
+shard_include("unittable-" .. game:GameName())
+shard_include("featuretable-" .. game:GameName())
 shard_include "commonfunctions"
