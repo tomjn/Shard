f5bc1266db381a289148a7245f3c27c8125aa50c
Tom J Nowell <contact@tomjn.com>
2017-05-11 11:01:42 +0100

    fix file loading when used as a LuaAI for boot.lua

--- data/ai/boot.lua
+++ data/ai/boot.lua
@@ -1,7 +1,7 @@
 -- @TODO: Move top level logic out of ai.lua into here, and then make it load environment specific boot.lua files
 
 if ShardSpringLua then
-	require "preload/spring_lua/boot"
+	VFS.Include( "luarules/gadgets/ai/preload/spring_lua/boot.lua" )
 elseif game_engine then
 	require "spring_cpp/boot"
 else
