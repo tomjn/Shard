c254d6cd5e48ab55ab3924995569cfceb82c8403
pandaro <prandipadaro@gmail.com>
2017-05-28 22:16:44 +0200

    transform common in a module

--- data/ai/BA/common.lua
+++ data/ai/BA/common.lua
@@ -1,17 +1,31 @@
-shard_include "unitlists"
-if ShardSpringLua then
-	if not unitTable or not featureTable then
-		unitTable, featureTable = shard_include("getunitfeaturetable")
-	end
-	if not CommonFunctionsLoaded then
+CommonHandler = class(Module)
+
+function CommonHandler:Name()
+	return "CommonHandler"
+end
+
+function CommonHandler:internalName()
+	return "commonhandler"
+end
+
+function CommonHandler:Init()
+
+	shard_include "unitlists"
+
+	if ShardSpringLua then
+		if not unitTable or not featureTable then
+			unitTable, featureTable = shard_include("getunitfeaturetable")
+		end
+		if not CommonFunctionsLoaded then
+			shard_include "commonfunctions"
+		end
+		if not UnitListsLoaded then
+			shard_include "unitlists"
+		end
+	else
+		shard_include "unitlists"
+		shard_include("unittable-" .. self.ai.game:GameName())
+		shard_include("featuretable-" .. self.ai.game:GameName())
 		shard_include "commonfunctions"
 	end
-	if not UnitListsLoaded then
-		shard_include "unitlists"
-	end
-else
-	shard_include "unitlists"
-	shard_include("unittable-" .. ai.game:GameName())
-	shard_include("featuretable-" .. ai.game:GameName())
-	shard_include "commonfunctions"
 end
